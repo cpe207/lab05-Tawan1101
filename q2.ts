286b3694dec5a226dbfd8440e75c1e0db3982195
Tawan1101 <wanchailungnoeng2019@gmail.com>
2024-07-06 19:12:37 +0700

    update1

--- q2.ts
+++ q2.ts
@@ -1,9 +1,14 @@
+interface Students1 {
+  name: string,
+  score: number;
+}
 // define interface for Student object
 /* Your code here */
 
 // assign interface/type to the function definition properly
-function findTopNames(students) {
+function findTopNames(students:Students1[]) {
   /* Your code here */
+  return students.filter(students => students.score > 8).map(students => students.name);
 }
 
 // assign interface/type to the student1 object properly
@@ -16,3 +21,6 @@ const students1 = [
 console.log(findTopNames(students1));
 
 module.exports = findTopNames;
+
+// รหัส นศ.: 660610793
+// ชื่อ-สกุล : วันฉาย ลุงเนิ่ง
